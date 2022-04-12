c3b235a9e64d4931df647dbbacf9cca544b96257
Felipe Monteiro Jácome <fmj1988@gmail.com>
2022-04-12 08:17:42 -0300

    ajuste metrica pandasGauge

--- api/src/app.py
+++ api/src/app.py
@@ -1,20 +1,22 @@
 from flask import Response, Flask, request
 import prometheus_client
 # from prometheus_client.core import CollectorRegistry
-from prometheus_client import Summary, Counter, Histogram, Gauge, Info, REGISTRY
+from prometheus_client import Summary, Counter, Histogram, Gauge, Info, REGISTRY, PandasGauge
 import random
-
+import pandas as pd
 import time
 
 app = Flask(__name__)
 
 _INF = float("inf")
-
+df = pd.DataFrame({'a': [1.1,2.2,3.3,4.4], 'b':[5.1,6.2,7.3,8.4], 'value': [1,2,3,4]})
 graphs = {}
 graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
 graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
 graphs['s'] = Summary('python_request_processing_seconds', 'Time spent processing request')
 graphs['info'] = Info('my_build_version', 'Description of info')
+PandasGauge('report_pandas', 'metric description', df=df, columns= ['a', 'b', 'value'], registry=REGISTRY)
+
 
 @app.route("/")
 def index():
